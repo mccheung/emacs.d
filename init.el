21e56682da9aa56862b56189cd8fdc337310bafa
mccheung <mc_cheung@126.com>
2016-08-11 21:15:43 +0800

    add other base setting

--- init.el
+++ init.el
@@ -1,3 +1,92 @@
+(package-initialize)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+    'package-archives
+    '("melpa" . "http://melpa.org/packages/")
+    t)
+(package-initialize))
+
+
+;; Fix some weird color escape sequences
+(setq system-uses-terminfo nil)
+
+;; Resolve symlinks
+(setq-default find-file-visit-truename t)
+
+;; Require a newline at the end of files
+(setq require-final-newline t)
+
+;; Search using regex by default, since that’s usually
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "M-%") 'query-replace-regexp)
+
+;; Single space still ends a sentence
+(setq sentence-end-double-space nil)
+
+;; Split windows a bit better (don’t split horizontally, I have a widescreen :P)
+(setq split-height-threshold nil)
+(setq split-width-threshold 180)
+
+;; Bury the *scratch* buffer, never kill it
+(defadvice kill-buffer (around kill-buffer-around-advice activate)
+  (let ((buffer-to-kill (ad-get-arg 0)))
+    (if (equal buffer-to-kill "*scratch*")
+        (bury-buffer)
+      ad-do-it)))
+
+;; Automatically revert file if it’s changed on disk
+(global-auto-revert-mode 1)
+;; be quiet about reverting files
+(setq auto-revert-verbose nil)
+
+;; Display the time and load on the modeline
+(setq
+ ;; don't display info about mail
+ display-time-mail-function (lambda () nil)
+ ;; update every 15 seconds instead of 60 seconds
+ display-time-interval 15)
+(display-time-mode 1)
+
+
+;; Set fill-column to 80 characters and set tab width to 4
+(setq-default fill-column 80)
+(setq-default default-tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+;; Ignore case when using completion for file names
+(setq read-file-name-completion-ignore-case t)
+
+;; hitting the y key to confirm
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; only on graphical Confirm before killing emacs-basic-display
+(when (window-system)
+  (setq confirm-kill-emacs 'yes-or-no-p))
+
+
+;; Always prefer UTF-8, anything else is insanity
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+
+;; global highlighting the current row
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#3e4446")
+(set-face-foreground 'highlight nil)
+
+;; Turn on syntax highlighting for all buffers
+(global-font-lock-mode t)
+
+
+;; Don’t beep
+(setq ring-bell-function (lambda ()))
+(setq inhibit-startup-screen t
+            initial-major-mode 'fundamental-mode)
+
 ;; stop creating backup~ files
 (setq make-backup-files nil) 
 
@@ -13,16 +102,9 @@
 ;;To disable the toolbar, use the following line:
 (tool-bar-mode -1) 
 
-;; disable welcome page
-(setq inhibit-startup-screen t)
-
-
-(require 'package)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-  (package-initialize)
-
-(require 'evil)
-(evil-mode 1)
+;; Vim like keybind
+;;(require 'evil)
+;;(evil-mode 1)
 
 ;; Auto-complete
 (ac-config-default)
@@ -40,34 +122,37 @@
 (require 'autopair)
 (autopair-global-mode)
 
+
+
 ;; Org mode
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-hook 'org-mode-hook 'turn-on-font-lock)
 (add-hook 'org-mode-hook
-	  (lambda()(setq truncate-lines nil)))
+          (lambda()(setq truncate-lines nil)))
 
 
-(setq org-publish-project-alist
-      '(("note-org"
-	 :base-directory "~/org-notes/"
-	 :publishing-directory "~/org-notes/publish"
-	 :base-extension "org"
-	 :recursive t
-	 :publishing-function org-publish-org-to-html
-	 :auto-index nil
-	 :index-filename "index.org"
-	 :index-title "index"
-	 :link-home "index.html"
-	 :section-numbers nil
-	 :style "<link rel=\"stylesheet\" href=\"./style/emacs.css\" type=\"text/css\"/>")
-	("note-static"
-	 :base-directory "~/org-notes/org"
-	 :publishing-directory "~/org-notes/publish"
-	 :recursive t
-	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el"
-	 :publishing-function org-publish-attachment)
-	("note"
-	 :components ("note-org" "note-static")
-	 :author "mc.cheung@aol.com")))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; 进度记录
+(setq org-log-done 'time)
+(setq org-log-done 'note)
+
+;; org tags setting
+(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) 
+                      ("@joris" . ?j) ("@pantuo" . ?p) 
+                      ("@learn" . ?l) ("@project" . ?p) 
+                      ("@english" . ?e)))
+
+;; Org-agenda config
+(eval-when-compile
+  (require 'org-agenda)
+  (require 'cl))
+
+
+;; themes
+;;(require 'color-theme)
+(load-theme 'spacemacs-dark t)
 
 
